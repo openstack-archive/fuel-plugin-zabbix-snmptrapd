8962ed056342d9fb979f2b54f6e3a796e9a23a37
Piotr Misiak <pmisiak@mirantis.com>
2015-06-05 13:49:38 +0200

    initial version of SNMP trap daemon for Zabbix plugin
    Change-Id: I4041c15fdf09af5944b75f142a5c374b24af380f

---
+++ deployment_scripts/puppet/modules/plugin_zabbix_snmptrapd/manifests/init.pp
@@ -0,0 +1,51 @@
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class plugin_zabbix_snmptrapd {
+
+  include plugin_zabbix_snmptrapd::params
+
+  $service_name    = $plugin_zabbix_snmptrapd::params::service_name
+  $server_ip       = hiera('management_vip')
+  $plugin_settings = hiera('zabbix_snmptrapd')
+
+  class { 'snmp':
+    snmptrapdaddr       => ["udp:${server_ip}:162"],
+    ro_community        => $plugin_settings['community'],
+    service_ensure      => 'stopped',
+    trap_service_ensure => 'running',
+    trap_service_enable => true,
+    trap_handlers       => ['default /usr/sbin/snmptthandler'],
+  }
+
+  file { "/etc/init.d/${service_name}":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => "puppet:///modules/plugin_zabbix_snmptrapd/initscripts/${service_name}",
+    require => Class['snmp'],
+    notify  => Service[$service_name],
+  }
+
+  class { 'plugin_zabbix_snmptrapd::snmptt':
+    require => Class['snmp'],
+  }
+
+  class { 'plugin_zabbix_snmptrapd::zabbix':
+    require => Class['plugin_zabbix_snmptrapd::snmptt'],
+  }
+
+}
